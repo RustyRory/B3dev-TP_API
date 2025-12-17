4d1f32d33e4cf28c8698786f461a0c884a8a6e0c
rustyrory <marada49@live.fr>
2025-12-17 13:56:42 +0100

    Fixes #38

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Déploiement automatique démarré..."
-cd /B3dev/B3dev-TP_API || exit
-git fetch origin main
-git reset --hard origin/main
-npm install
-npm start
-echo "Déploiement terminé."
