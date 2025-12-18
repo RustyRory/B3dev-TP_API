641f34ca52479b2412155ab66058a455129dc43a
rustyrory <marada49@live.fr>
2025-12-18 16:00:45 +0100

    Fixes #60

--- serveurExpress/src/app.js
+++ serveurExpress/src/app.js
@@ -231,6 +231,6 @@ io.on("connection", (socket) => {
   );
 });
 
-httpServer.listen(PORT, "127.0.0.1", () =>
-  console.log(`Serveur démarré sur http://127.0.0.1:${PORT}`)
+httpServer.listen(PORT, "78.138.58.95", () =>
+  console.log(`Serveur démarré sur http://78.138.58.95:${PORT}`)
 );
